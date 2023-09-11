a2cf29ed71c9889327aba624c9f55cd73f9d0028
Roshni V R <vrroshni2001@gmail.com>
2023-09-11 15:55:28 +0530

    designed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-
+import Header from "./components/Header"
 
 function App() {
- 
+
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className="w-full h-full" >
+      <Header />
+
+
+
+
+    </div>
   )
 }
 
